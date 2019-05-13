70451f09abdb7402bcf6c42c88d6510560b203cc
xiaohun's PC <xiaohunsdt@gmail.com>
2019-05-13 23:31:50 +0900

    change project structure and add rtmp server project

---
+++ end-admin/rtmp/src/main/java/net/novaborn/RtmpServerApp.java
@@ -0,0 +1,42 @@
+package net.novaborn;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @author xiaohun
+ * Date: 2019/5/13
+ * Time: 23:22
+ * Description: main class
+ */
+public class RtmpServerApp {
+    private static int PORT = 1935;
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup).channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+//                        ch.pipeline().addLast(new HelloServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture future = bootstrap.bind(PORT).sync();
+            future.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
